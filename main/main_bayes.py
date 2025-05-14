25b8799e0355c7bcfbb2e04d68671b593cbe39c8
TranSon2707 <trantienson2707@gmail.com>
2025-05-14 21:30:10 +0700

    Delete main directory

--- main/main_bayes.py
+++
@@ -1,6 +0,0 @@
-from src.bayes.Utils.predict import predict_email
-
-
-# Example usage
-email = "Congratulations! You have won a free iPhone. Click here to claim it."
-print("Prediction:", predict_email(email))
