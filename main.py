6a7a6dc285c2e090996666893418c13c287fef8a
TranSon2707 <trantienson2707@gmail.com>
2025-05-17 11:14:21 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.Utils.predict import predict_email, main_predict
+from src.Utils.predict import main_predict
 
 if __name__ == "__main__":
     main_predict()
--- src/Utils/predict.py
+++ src/Utils/predict.py
@@ -4,12 +4,13 @@ import joblib
 
 def predict_email(text, model = 'bayes'):
 
+
     if model == 'bayes':
         model = joblib.load('models/bayes/spam_classifier_model.pkl')
         vectorizer = joblib.load('models/bayes/tfidf_vectorizer.pkl')
     elif model == 'isolation_forest':
-        model = joblib.load('models/isolation_forest/spam_classifier_model.pkl')
-        vectorizer = joblib.load('models/isolation_forest/tfidf_vectorizer.pkl')    
+        model = joblib.load('models/isolation_forest/spam_classifier_model_1.pkl')
+        vectorizer = joblib.load('models/isolation_forest/tfidf_vectorizer_1.pkl')    
     elif model == 'logistic_regression':
         model = joblib.load('models/logistic_regression/spam_classifier_model.pkl')
         vectorizer = joblib.load('models/logistic_regression/tfidf_vectorizer.pkl')
@@ -28,8 +29,8 @@ def predict_email(text, model = 'bayes'):
 
 def main_predict():
     
-    input_model = input("Enter model number or name \n(1 bayes/2 isolation_forest/3 logistic_regression): ").split()
-    input_method = input("Enter method number or name \n(1 by hand/2 txt/3 csv): ")
+    input_model = input("Enter model number or name \n(1 bayes/2 isolation_forest/3 logistic_regression): ").strip().split()
+    input_method = input("Enter method number or name \n(1 by hand/2 txt/3 csv): ").strip()
 
     if input_method == '1' or input_method.lower() == 'by hand':
         input_email = input("Enter the email text: ")
@@ -37,16 +38,16 @@ def main_predict():
         if '1' in input_model or 'bayes' in input_model:
             print("Bayes Model Prediction:", end=" ")
             predict_email(input_email, model='bayes')
-        elif '2' in input_model or 'isolation_forest' in input_model:
+        if '2' in input_model or 'isolation_forest' in input_model:
             print("Isolation Forest Model Prediction:", end=" ")
             predict_email(input_email, model='isolation_forest')
-        elif '3' in input_model or 'logistic_regression' in input_model:
+        if '3' in input_model or 'logistic_regression' in input_model:
             print("Logistic Regression Model Prediction:", end=" ")
             predict_email(input_email, model='logistic_regression')
 
 
 
-    elif input_method == '2' or input_method.lower() == 'txt':
+    if input_method == '2' or input_method.lower() == 'txt':
         input_file = input("Enter the path to the .txt file: ")
         with open(input_file, 'r') as f:
             email_text = f.read()
@@ -54,16 +55,16 @@ def main_predict():
         if '1' in input_model or 'bayes' in input_model:
             print("Bayes Model Prediction:", end=" ")
             predict_email(email_text, model='bayes')
-        elif '2' in input_model or 'isolation_forest' in input_model:
+        if '2' in input_model or 'isolation_forest' in input_model:
             print("Isolation Forest Model Prediction:", end=" ")
             predict_email(email_text, model='isolation_forest')
-        elif '3' in input_model or 'logistic_regression' in input_model:
+        if '3' in input_model or 'logistic_regression' in input_model:
             print("Logistic Regression Model Prediction:", end=" ")
             predict_email(email_text, model='logistic_regression')
 
 
 
-    elif input_method == '3' or input_method.lower() == 'csv':
+    if input_method == '3' or input_method.lower() == 'csv':
         input_file = input("Enter the path to the .csv file: ")
         df = pd.read_csv(input_file)
         
@@ -72,12 +73,12 @@ def main_predict():
             for index, row in df.iterrows():
                 print(f"Email {index}:", end=" ")
                 predict_email(row[0], model='bayes')
-        elif '2' in input_model or 'isolation_forest' in input_model:
+        if '2' in input_model or 'isolation_forest' in input_model:
             print("Isolation Forest Model Prediction:")
             for index, row in df.iterrows():
                 print(f"Email {index}:", end=" ")
                 predict_email(row[0], model='isolation_forest')
-        elif '3' in input_model or 'logistic_regression' in input_model:
+        if '3' in input_model or 'logistic_regression' in input_model:
             print("Logistic Regression Model Prediction:")
             for index, row in df.iterrows():
                 print(f"Email {index}:", end=" ")
